82363f318572c66895ba4f6a86a2d74559e4c185
natalia galindo <nataliadgalindo@outlook.com>
2023-10-03 10:29:00 +0100

    update img src

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
-import dividerImg from "../images/pattern-divider-desktop.svg"
-import dice from "../images/icon-dice.svg"
+/* eslint-disable react/no-unescaped-entities */
+import dividerImg from "/images/pattern-divider-desktop.svg"
 import axios from "axios"
 import { useState, useEffect } from "react"
 
 function App() {
   let [advice, setAdvice] = useState("")
+
   useEffect(() => {
     handleButtonClick()
   }, [])
@@ -21,18 +22,20 @@ function App() {
   }
 
   return (
-    <main className="advice-box">
-      <div className="advice-content">
-        <p className="advice-id">Advice #{advice.id}</p>
-        <h1 className="advice">"{advice.advice}"</h1>
-        <div className="divider-img">
-          <img className="divider" src={dividerImg} alt="divider" />
-        </div>
-        <button onClick={handleButtonClick} className="advice-btn">
-          <img src={dice} alt="dice" />
-        </button>
-      </div>
-    </main>
+    <div className="advice-container">
+      <main className="advice-box">
+        <section className="advice-content">
+          <p className="advice-id">Advice #{advice.id}</p>
+          <h1 className="advice">"{advice.advice}"</h1>
+          <div className="divider-img">
+            <img className="divider" src={dividerImg} alt="divider" />
+          </div>
+          <button onClick={handleButtonClick} className="advice-btn">
+            <img src={"/images/icon-dice.svg"} alt="dice" />
+          </button>
+        </section>
+      </main>
+    </div>
   )
 }
 export default App
